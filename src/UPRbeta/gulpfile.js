6a8d01715659806e8c9ef77b3159fc22930ccf83
aecuman <aecuman24@gmail.com>
2016-08-02 15:35:11 -0700

    minfix

--- src/UPRbeta/gulpfile.js
+++ src/UPRbeta/gulpfile.js
@@ -1,5 +1,4 @@
-﻿/// <binding Clean='clean' />
-
+/// <binding BeforeBuild='min, min:css, min:js' Clean='clean' />
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
